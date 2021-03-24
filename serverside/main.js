7c52c1031071e6977c3a6c83d7df94dcaaf6cda9
Sanjay Krishna <krishn89@purdue.edu>
2021-03-24 14:03:46 -0400

    deleting

--- serverside/main.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 23556;
-
-app.get('/', (req, res) => {
-    res.send("Welcome to managr");
-});
-
-app.listen(PORT, () => {"listening on port " + PORT});
