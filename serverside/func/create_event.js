733b3b39861c2fc88c54d380b36876ab31c13554
Anvi Mehta <mehta109@purdue.edu>
2021-04-25 21:59:23 -0400

    removing redundant code

--- serverside/func/create_event.js
+++ serverside/func/create_event.js
@@ -36,7 +36,6 @@ const createEvent = (data, connection) => {
     var event_id = Math.floor(Math.random() * 10000);
     var query1 = `SELECT * FROM events WHERE event_id = '${event_id}' OR (date='${evdate}' AND room_id='${room_id}');`
     var query2 = `INSERT INTO events (event_id, date, host_id, room_id, event_title, details, invited_count, accepted_count, cancel) VALUES (${event_id}, '${evdate}', 1, ${room_id}, '${event_title}', '${details}', 0, 0, 0);`
-    // var query3 = `SELECT * FROM events WHERE date='${evdate}' AND room_id='${room_id}';`
     return new Promise(async (resolve, reject) => {
         connection.query(query1, (err, result) => {
             if (err) {
