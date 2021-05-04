ded3f45a3d0caffc0374824fbd05ed2a1581f423
Anvi Mehta <mehta109@purdue.edu>
2021-05-04 19:29:40 -0400

    invite people

--- serverside/func/get_user_invites.js
+++ serverside/func/get_user_invites.js
@@ -16,7 +16,7 @@ process.on("message", message => {
 });
 
 const getUserInvites = (person_id, connection) => {
-    var query = `SELECT * FROM invitelist natural join rsvp WHERE person_id = ${person_id}`;
+    var query = `SELECT rsvp.event_id, rsvp.person_id, rsvp.attending, invitelist.email, invitelist.event_title, events.details, events.room_id, events.date, rooms.building, rooms.room FROM invitelist natural join rsvp join events on (rsvp.event_id=events.event_id) join rooms ON (events.room_id=rooms.room_id) WHERE person_id = ${person_id};`;
     return new Promise(async (resolve, reject) => {
         await connection.query(query, (err, result) => {
             if (err) {
