88690298d7420a7d0ca9f13bc1d3d982bfe3222e
Sanjay Krishna <krishn89@purdue.edu>
2021-05-04 20:40:03 -0400

    made some changes to rsvpCard.dart and invitation.dart

--- managr_frontend/lib/customWidgets/rsvpCard.dart
+++ managr_frontend/lib/customWidgets/rsvpCard.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 class RsvpCard extends StatefulWidget {
   var eventTitle;
@@ -8,19 +10,32 @@ class RsvpCard extends StatefulWidget {
   var building;
   var room;
 
-  RsvpCard(this.eventTitle, this.eventDetails, this.date, this.rsvp,
-      this.building, this.room);
+  RsvpCard(this.eventTitle, this.eventDetails, this.date, this.building,
+      this.room, this.rsvp);
 
   @override
   _RsvpCardState createState() => _RsvpCardState();
 }
 
 class _RsvpCardState extends State<RsvpCard> {
+  SharedPreferences prefs;
+  Future<http.Response> getEvent() async {
+    prefs = await SharedPreferences.getInstance();
+    int userID = prefs.getInt(widget.rsvp);
+    var url = "http://managr-server.herokuapp.com/invitation?attending=" +
+        userID.toString();
+    print("userID is " + userID.toString());
+    http.Response resp = await http.get(url);
+    print("response body is ${resp.body}");
+    return resp;
+  }
+
   @override
   Widget build(BuildContext context) {
     var screenWidth = MediaQuery.of(context).size.width;
     var screenHeight = MediaQuery.of(context).size.height;
 
+    var _hasBeenPressed = false;
     return Container(
       child: GestureDetector(
         child: Card(
@@ -69,11 +84,27 @@ class _RsvpCardState extends State<RsvpCard> {
                     )),
                 Container(
                     margin: EdgeInsets.only(top: 20),
-                    child: Text(
-                      "RSVP: " + widget.rsvp.toString(),
-                      style: TextStyle(
-                          fontSize: 15, fontWeight: FontWeight.normal),
-                    )),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            "RSVP: " + widget.rsvp.toString(),
+                            style: TextStyle(
+                                fontSize: 15, fontWeight: FontWeight.normal),
+                          ),
+                          RaisedButton(
+                            child: new Text('Attend Event'),
+                            textColor: Colors.white,
+                            // 2
+                            color: widget.rsvp == 0 ? Colors.green : Colors.red,
+                            // 3
+                            onPressed: () => {
+                              setState(() {
+                                _hasBeenPressed = !_hasBeenPressed;
+                              })
+                            },
+                          )
+                        ])),
               ],
             ),
           ),
