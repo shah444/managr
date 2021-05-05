a03b0b6b0da8e7e37694de97d906422587a25615
Vidit Shah <viditshah@Vidits-MacBook-Pro.local>
2021-05-05 07:29:05 -0400

    Account deletion now also deletes all active events of the user.

--- serverside/func/delete_account.js
+++ serverside/func/delete_account.js
@@ -29,32 +29,35 @@ const deleteAccount = (data, connection) => {
     var ID = data.ID;
     var query1 = `SELECT * FROM users WHERE person_id = '${ID}';`
     var query2 = `DELETE FROM users WHERE person_id = '${ID}';`
+    var query3 = `DELETE FROM events WHERE host_id = ${ID}`;
     return new Promise(async (resolve, reject) => {
-        connection.query(query1, (err, result) => {
+        connection.query(query1, async (err, result) => {
             if (err) {
                 console.log(err.message);
                 reject(err.message);
             }
-            try {
-                if (result.length < 1) {
-                    process.send({"Error": "The account does not exist"});
-                    resolve("Account does not exist");
-                }
-                else {
-                    connection.query(query2, (err, result) => {
-                        if(err){
-                            console.log(err);
-                            reject(err.message);
-                        }
-                        resolve("deleted");
-                    });
+
+            if (result.length < 1) {
+                process.send({"Error": "The account does not exist"});
+                resolve("Account does not exist");
+            }
+            else {
+                await connection.query(query2, async (err1, result1) => {
+                    if(err) {
+                        console.log(err1);
+                        reject(err1.message);
                     }
-                }
-                catch(error){
-                    reject(err.message);
-                }
-            console.log(result);
-            resolve(result);
+
+                    await connection.query(query3, (err2, result2) => {
+                        if (err2) {
+                            console.log(err2.message);
+                            reject(err2.message);
+                        }
+                        resolve(result2);
+                    });
+                    
+                });
+            }
         });
     });
 };
