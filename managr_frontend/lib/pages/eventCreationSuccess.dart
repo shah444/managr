6fc87377b0b5f6baed30a06d0c79178e45ef0ec8
Sanjay Krishna <krishn89@purdue.edu>
2021-05-05 17:48:29 -0400

    frontend for invite users to event

--- managr_frontend/lib/pages/eventCreationSuccess.dart
+++ managr_frontend/lib/pages/eventCreationSuccess.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:managr_frontend/colors.dart';
 import 'package:managr_frontend/pages/homepage.dart';
+import 'package:managr_frontend/pages/inviteUsers.dart';
+
+import 'package:http/http.dart' as http;
 
 class EventCreationSuccess extends StatefulWidget {
   @override
@@ -8,6 +13,26 @@ class EventCreationSuccess extends StatefulWidget {
 }
 
 class _EventCreationSuccessState extends State<EventCreationSuccess> {
+  /*
+  TextEditingController emailController = new TextEditingController();
+
+  Future<void> inviteUsers() async {
+    var email = emailController.text;
+
+    var url = "http://managr-server.herokuapp.com/invitelist";
+    var accDetails = JsonEncoder().convert({"email": emailController.text});
+    http.Response resp = await http.post(url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: accDetails);
+
+    if (resp.statusCode == 200) {
+      print("User information added into the database successfully");
+    }
+  }
+  */
+
   @override
   Widget build(BuildContext context) {
     var screenWidth = MediaQuery.of(context).size.width;
@@ -30,8 +55,14 @@ class _EventCreationSuccessState extends State<EventCreationSuccess> {
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              Text("Congratulations!", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-              Text("Your event has been successfully created.", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+              Text(
+                "Congratulations!",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Your event has been successfully created.",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
               Container(
                 margin: EdgeInsets.only(top: 50),
                 child: Text("Your event currently has 0 invitees."),
@@ -40,13 +71,18 @@ class _EventCreationSuccessState extends State<EventCreationSuccess> {
                 padding: EdgeInsets.only(top: 20),
                 child: ButtonTheme(
                   buttonColor: buttonColor,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
                   minWidth: screenWidth / 5,
                   child: RaisedButton(
                     elevation: 2,
                     clipBehavior: Clip.antiAlias,
                     child: Text("Invite Guests"),
                     onPressed: () async {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => InviteUsers()));
                     },
                   ),
                 ),
@@ -55,14 +91,16 @@ class _EventCreationSuccessState extends State<EventCreationSuccess> {
                 padding: EdgeInsets.only(top: 20),
                 child: ButtonTheme(
                   buttonColor: buttonColor,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
                   minWidth: screenWidth / 5,
                   child: RaisedButton(
                     elevation: 2,
                     clipBehavior: Clip.antiAlias,
                     child: Text("Back to home page"),
                     onPressed: () async {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => HomePage()));
                     },
                   ),
                 ),
---
+++ managr_frontend/lib/pages/inviteUsers.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import '../colors.dart';
+import 'package:http/http.dart' as http;
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class InviteUsers extends StatefulWidget {
+  @override
+  _InviteUsersState createState() => _InviteUsersState();
+}
+
+class _InviteUsersState extends State<InviteUsers> {
+  TextEditingController emailController = new TextEditingController();
+
+  Future<void> inviteUsers() async {
+    var email = emailController.text;
+
+    var url = "http://managr-server.herokuapp.com/invitelist";
+    var accDetails = JsonEncoder().convert({"email": emailController.text});
+    http.Response resp = await http.post(url,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: accDetails);
+
+    if (resp.statusCode == 200) {
+      print("User information added into the database successfully");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenWidth = MediaQuery.of(context).size.width;
+    var screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: bottomGrad,
+      appBar: new AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                width: screenWidth * 0.8,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(9),
+                ),
+                child: TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                      icon: Icon(Icons.email), hintText: "Guest Email"),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.only(top: 30),
+                child: ButtonTheme(
+                  minWidth: screenWidth / 3,
+                  height: screenHeight / 16,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30)),
+                  buttonColor: buttonColor,
+                  child: RaisedButton(
+                    child: Text("Invite Guest to Event"),
+                    onPressed: () async {
+                      await inviteUsers();
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
