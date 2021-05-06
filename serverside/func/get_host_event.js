4073aba72f58201ba0f1c71324a9bc9b21be4ad1
Sanjay Krishna <krishn89@purdue.edu>
2021-05-05 22:46:02 -0400

    changed the date system in get_host_event

--- serverside/func/get_host_event.js
+++ serverside/func/get_host_event.js
@@ -5,7 +5,6 @@ process.on("message", message => {
         if (err) {
             return console.log('err: ' + err.message);
         }
-        
         console.log('Database connection established.');
         getHostEvent(message.host_id, message.date, message.room, connection).then((result) => {
             connection.release();
@@ -16,8 +15,14 @@ process.on("message", message => {
 });
 
 const getHostEvent = (host_id, date, room, connection) => {
+    var evdate = new Date(date).toISOString().substring(0,10);
+    evdate += " 00:00:00";
+    console.log(evdate);
+
+    //evdate.setHours(0,0,0);
+    console.log(evdate);
     var query =  `SELECT event_id FROM events \
-                    WHERE host_id = '${host_id}' and date = '${date}' and room_id = '${room}' \
+                    WHERE host_id = '${host_id}' and date = '${evdate}' and room_id = '${room}' \
                     LIMIT 1;`;
    // var query =  `Select * From events Select min(date) as mn FROM (SELECT * FROM invitelist natural join events WHERE person_id = ${person_id}) where date = mn`;
     //var query = `SELECT * FROM invitelist natural join events WHERE person_id = ${person_id} and date = '2008-11-09 15:45:21'`;// (SELECT min(date) FROM events))`;
